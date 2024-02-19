25c180561b0ce9b91cf87792b6097be31344aeb0
Trine <trla0001@stud.kea.dk>
2024-02-19 14:59:49 +0100

    java rettet

--- js/productlist.js
+++ js/productlist.js
@@ -1,7 +1,7 @@
 const url = "https://mymxopdswecfhtqjjdaj.supabase.co";
 const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15bXhvcGRzd2VjZmh0cWpqZGFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4MTc1NzksImV4cCI6MjAyMzM5MzU3OX0.7qo3PlF20tNiwd1eHKnvFFIqGTxqqFUHABvXwE-T_fc";
 
-fetch(url + "/rest/v1/Svampe?select=name,svampe_img", {
+fetch(url + "/rest/v1/Svampe?select=id,name,svampe_img", {
   method: "GET",
   headers: {
     apikey: key,
@@ -23,12 +23,11 @@ function showProduct(product) {
   const copy = template.cloneNode(true);
 
   //ændre indhold
-  copy.querySelector(".mushroom_img").src = `https://vildmadv2.vps.webdock.io/application/files/7516/2452/5088/Svovlporresvamp_ravarekort_app.png`;
+  copy.querySelector(".mushroom_img").src = product.svampe_img;
   copy.querySelector(".mushroom_name").textContent = product.name;
 
   // Link
   copy.querySelector(".mushroom_link").href = `/product.html?id=${product.id}`;
-  //copy.querySelector(".mushroom_link").setAttribute("href", `/rest/v1/Svampe?select=id${product.id}`);
 
   // Tilføj layout tilfældigt
   const randomNumber = Math.random(); // Generer et tilfældigt tal mellem 0 og 1
