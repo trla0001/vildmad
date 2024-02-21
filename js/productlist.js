b0f8e99a24fedcde64a09d6d124a54bfbe4bb211
Trine <trla0001@stud.kea.dk>
2024-02-21 10:58:29 +0100

    Sidste ændringer tilføjet

--- js/productlist.js
+++ js/productlist.js
@@ -33,9 +33,24 @@ function getSvampe() {
     //ændre indhold
     copy.querySelector(".mushroom_img").src = product.svampe_img;
     copy.querySelector(".mushroom_name").textContent = product.name;
+    copy.querySelector(".mushroom_name").textContent = product.name;
 
     // Link
     copy.querySelector(".mushroom_link").href = `/product.html?id=${product.id}`;
+
+    // Fjern alle tidligere sæsonklasser
+    copy.querySelector(".mushroom_article").classList.remove("spring", "summer", "autumn", "winter");
+
+    // Tilføj klasse baseret på sæson
+    if (season === "Spring") {
+      copy.querySelector(".mushroom_article").classList.add("spring");
+    } else if (season === "Summer") {
+      copy.querySelector(".mushroom_article").classList.add("summer");
+    } else if (season === "Autumn") {
+      copy.querySelector(".mushroom_article").classList.add("autumn");
+    } else if (season === "Winter") {
+      copy.querySelector(".mushroom_article").classList.add("winter");
+    }
 
     //appende
     document.querySelector(".gallery").appendChild(copy);
